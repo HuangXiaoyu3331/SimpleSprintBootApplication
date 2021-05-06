585a15a882f5ebcada0f90b7a3818343854ca236
huangxy <1076229832@qq.com>
2021-05-06 21:47:57 +0800

    test sh

--- Jenkinsfiles/build.groovy
+++ Jenkinsfiles/build.groovy
@@ -4,6 +4,7 @@ pipeline {
     stages {
         stage('repoClone') {
             steps {
+                sh "echo hello sh"
                 echo 'repoClone...'
                 echo "Running ${env.BUILD_ID} ${env.BUILD_DISPLAY_NAME} on ${env.NODE_NAME} and JOB ${env.JOB_NAME}"
             }
