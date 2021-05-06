46326f3b28a5405ceff3aff3a5ca64e3f334f985
huangxy <1076229832@qq.com>
2021-05-06 22:48:58 +0800

    test git plugin

--- Jenkinsfiles/build.groovy
+++ Jenkinsfiles/build.groovy
@@ -4,9 +4,8 @@ pipeline {
     stages {
         stage('repoClone') {
             steps {
-                sh "echo hello sh"
-                echo 'repoClone...'
-                echo "Running ${env.BUILD_ID} ${env.BUILD_DISPLAY_NAME} on ${env.NODE_NAME} and JOB ${env.JOB_NAME}"
+                checkout([$class: 'GitSCM', branches: [[name: 'main']],
+                          userRemoteConfigs: [[url: 'https://github.com/HuangXiaoyu3331/SimpleSprintBootApplication']]])
             }
         }
     }
